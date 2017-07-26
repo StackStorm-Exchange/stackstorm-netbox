b8b9bdf55fac359135794ccf5e5e50d60b0fe25d
whites2 <whites2@g.cofc.edu>
2017-07-26 10:19:40 -0400

    POST Request Support and new actions for netbox 2.1.0 (#4)
    * Added action to get available IPs within a prefix
    
    * Added action for available IP POST request
    
    * Bumped version number to 0.2.1
    
    * changed version number as per request
    
    * minor documentation fixes
    
    * minor fixes for circleci
    
    * removed extraneous whitespace
    
    * removed more extraneous whitespace
    
    * removed all of the extraneous whitespace
    
    * 'fixed' email address
    
    * removed email address

---
+++ actions/base_post_action.py
@@ -0,0 +1,12 @@
+
+from lib.action import NetboxBaseAction
+
+
+class NetboxBasePostAction(NetboxBaseAction):
+    """Base get action"""
+
+    def run(self, endpoint_uri, **kwargs):
+        """Base get action
+        endpoint_uri is pased from metadata file
+        """
+        return self.post(endpoint_uri, **kwargs)
--- actions/lib/action.py
+++ actions/lib/action.py
@@ -15,11 +15,9 @@ class NetboxBaseAction(Action):
     def __init__(self, config):
         super(NetboxBaseAction, self).__init__(config)
 
-    def get(self, endpoint_uri, **kwargs):
-        """Make a get request to the API URI passed in
+    def _make_request(self, endpoint_uri, http_action, **kwargs):
+        """Logic to make all types of requests
         """
-
-        self.logger.debug("Calling base get with kwargs: {}".format(kwargs))
 
         if self.config['use_https']:
             url = 'https://'
@@ -38,6 +36,24 @@ class NetboxBaseAction(Action):
             kwargs['id__in'] = ','.join(kwargs['id__in'])
             self.logger.debug('id__in transformed to {}'.format(kwargs['id__in']))
 
-        r = requests.get(url, verify=self.config['ssl_verify'], headers=headers, params=kwargs)
+        if http_action == "GET":
+            self.logger.debug("Calling base get with kwargs: {}".format(kwargs))
+            r = requests.get(url, verify=self.config['ssl_verify'], headers=headers, params=kwargs)
+
+        elif http_action == "POST":
+            self.logger.debug("Calling base post with kwargs: {}".format(kwargs))
+            r = requests.post(url, verify=self.config['ssl_verify'], headers=headers, data=kwargs)
 
         return {'raw': r.json()}
+
+    def get(self, endpoint_uri, **kwargs):
+        """Make a get request to the API URI passed in
+        """
+
+        return self._make_request(endpoint_uri, "GET", **kwargs)
+
+    def post(self, endpoint_uri, **kwargs):
+        """Make a post request to the API URI passed in
+        """
+
+        return self._make_request(endpoint_uri, "POST", **kwargs)
