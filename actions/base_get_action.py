68235a8823c6704b324961b57480423a9806c420
John Anderson <lampwins@gmail.com>
2017-10-09 17:06:48 -0400

    Allow saving results in st2 key/value store (#6)
    * first pass on storing results in keystore
    
    * updated keynames
    
    * added params to all get actions
    
    * added changelog entry and version bumb

--- actions/base_get_action.py
+++ actions/base_get_action.py
@@ -1,5 +1,9 @@
 
+import json
+
 from lib.action import NetboxBaseAction
+from st2client.client import Client
+from st2client.models import KeyValuePair
 
 
 class NetboxBaseGetAction(NetboxBaseAction):
@@ -9,4 +13,19 @@ class NetboxBaseGetAction(NetboxBaseAction):
         """Base get action
         endpoint_uri is pased from metadata file
         """
+
+        if kwargs.get('save_in_key_store') and not kwargs.get('save_in_key_store_key_name'):
+            return (False, 'save_in_key_store_key_name MUST be used with save_in_key_store!')
+
+        result = self.get(endpoint_uri, **kwargs)
+
+        if kwargs['save_in_key_store']:
+            # save the result in the st2 keystore
+            client = Client(base_url='http://localhost')
+            key_name = kwargs['save_in_key_store_key_name']
+            client.keys.update(KeyValuePair(name=key_name, value=json.dumps(result),
+                                            ttl=kwargs['save_in_key_store_ttl']))
+
+            return (True, "Result stored in st2 key {}".format(key_name))
+
         return self.get(endpoint_uri, **kwargs)
