809a09bc168020c34838594d27a67d56fb6e41f2
Carlos <nzlosh@yahoo.com>
2025-06-13 11:18:21 +0200

    Update base code to support http return status.

--- actions/run.py
+++ actions/run.py
@@ -6,35 +6,54 @@ from st2client.models import KeyValuePair
 
 
 class NetboxHTTPAction(NetboxBaseAction):
-    """Base run action"""
+    """
+    Action to call netbox api and return response.
+    """
 
-    def run(self, endpoint_uri, http_verb, get_detail_route_eligible, **kwargs):
-        """Base run action
+    def run(self, endpoint_uri, http_verb, get_detail_route_eligible, fail_non_2xx, **kwargs):
         """
-
-        if http_verb == 'get':
-            if kwargs.get('id', False) and get_detail_route_eligible:
+        StackStorm action entry point.
+        """
+        if http_verb == "get":
+            if kwargs.get("id", False) and get_detail_route_eligible:
                 # modify the `endpoint_uri` to use the detail route
-                endpoint_uri = '{}{}/'.format(endpoint_uri, str(kwargs.pop('id')))
+                endpoint_uri = "{}{}/".format(endpoint_uri, str(kwargs.pop("id")))
                 self.logger.debug(
-                    'endpoint_uri transformed to {} because id was passed'.format(endpoint_uri)
+                    "endpoint_uri transformed to {} because id was passed".format(endpoint_uri)
                 )
 
-            if kwargs.get('save_in_key_store') and not kwargs.get('save_in_key_store_key_name'):
-                return (False, 'save_in_key_store_key_name MUST be used with save_in_key_store!')
+            if kwargs.get("save_in_key_store") and not kwargs.get("save_in_key_store_key_name"):
+                return (False, "save_in_key_store_key_name MUST be used with save_in_key_store!")
 
             result = self.make_request(endpoint_uri, http_verb, **kwargs)
 
-            if kwargs['save_in_key_store']:
+            if fail_non_2xx:
+                # Return error rather than storing it in the keystone.
+                if result["status"] not in range(200, 300):
+                    return (False, result)
+
+            if kwargs["save_in_key_store"]:
                 # save the result in the st2 keystore
-                client = Client(base_url='http://localhost')
-                key_name = kwargs['save_in_key_store_key_name']
-                client.keys.update(KeyValuePair(name=key_name, value=json.dumps(result),
-                                                ttl=kwargs['save_in_key_store_ttl']))
+                client = Client(base_url="http://localhost")
+                key_name = kwargs["save_in_key_store_key_name"]
+                client.keys.update(
+                    KeyValuePair(
+                        name=key_name, value=json.dumps(result), ttl=kwargs["save_in_key_store_ttl"]
+                    )
+                )
 
                 return (True, "Result stored in st2 key {}".format(key_name))
 
         else:
             result = self.make_request(endpoint_uri, http_verb, **kwargs)
 
-        return result
+        # To maintain backward compatibility, this action always returns True (success) event
+        # when the netbox http return status is not OK.  action_succeeded is set to True and
+        # will only be set to False if the `fail_non_2xx` argument is set to True _and_ netbox
+        # http return code is not 2xx.
+        action_succeeded = True
+
+        if fail_non_2xx:
+            action_succeeded = result.get("status") in range(200, 300)
+
+        return (action_succeeded, result)
